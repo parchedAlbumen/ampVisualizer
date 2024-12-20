2d506768020c20fb9871670df076b3a18f3ab458
parchedAlbumen <118156055+parchedAlbumen@users.noreply.github.com>
2024-12-20 12:38:25 -0800

    Add files via upload
    code and file of what I am trying to do

---
+++ main.py
@@ -0,0 +1,18 @@
+import librosa
+
+#converts the file to time series and sampling rate (whatever these means)
+#time series - an audio signal denoted by y; y[t] is amplitude of the waveform at sample t???
+#sampling rate - number of samples per second of a time series ??
+y, sr = librosa.load("anong alam.mp3")
+
+#grabs the tempo and beat frame (wtv beatframe mean)
+tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+
+#duration 
+duration = librosa.get_duration(y=y, sr=sr)
+print(duration)
+
+#beat_time is what
+beat_times = librosa.frames_to_time(beat_frames, sr=sr)
+
+print("hello")
